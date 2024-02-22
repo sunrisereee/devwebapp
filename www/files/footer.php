b7c7f8b4591ca170a87fb0583eaebd3da49fcc8a
sunrisereee <gkarpeykin@mail.ru>
2024-02-22 10:33:08 +0300

    Add some php files

---
+++ www/files/footer.php
@@ -0,0 +1,9 @@
+<php>
+    <footer class="background-color align-text">
+        <div class ="wrapper">
+            <h2>
+                Разработка веб-приложений, 2024
+            </h2>
+        </div>
+    </footer>
+</php>
